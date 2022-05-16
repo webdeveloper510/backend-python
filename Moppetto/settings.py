76a2b15676b6bf7ab43a5d3620ff086446bd2391
webdeveloper510 <developerweb6@gmail.com>
2022-05-16 09:53:49 +0530

    again

--- Moppetto/settings.py
+++ Moppetto/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist',
+    # 'rest_framework_simplejwt.token_blacklist',
     # 3rd party package
     # apps
     'corsheaders',
